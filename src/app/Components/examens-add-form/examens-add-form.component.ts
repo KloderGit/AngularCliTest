adcb5a0dc4695038d743d1fc68c42ab8aee620b2
Kloder <kloder3@gmail.com>
2017-03-07 17:39:04 +0300

    Скелет компонента

---
+++ src/app/Components/examens-add-form/examens-add-form.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'examens-add-form',
+	templateUrl: 'examens-add-form.component.html'
+})
+
+export class ExamensAddFormComponent implements OnInit {
+
+	ngOnInit() { }
+}
